0c4b02cf553b77362d9947de041cefd0e46ca2a7
kaushal <kaushalc64@gmail.com>
2023-07-11 09:23:56 +0530

    first_commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import HomePage from "@/components/HomePage";
+import Navbar from "@/components/Navbar";
+import Upload from "@/components/Upload";
+
+
+export default function Home() {
+  return (
+    <main >
+      <Navbar />
+      <HomePage />
+      <Upload />
+    </main>
+  )
+}
